a2983d0708acc5c2235a9376f075d6baf2108539
yohanpratamaa <yohanpratamaa@gmail.com>
2025-03-24 10:36:56 +0700

    create file main.jsx to call the function HelloWorld.jsx

---
+++ learn_react/src/hello-world/main.jsx
@@ -0,0 +1,13 @@
+import { createRoot } from "react-dom/client";
+import HelloWorld from "./HelloWorld.jsx";
+import { StrictMode } from "react";
+
+
+createRoot(document.getElementById("root"))
+    .render(
+        
+        <StrictMode>
+            <HelloWorld />
+        </StrictMode>
+
+    );
