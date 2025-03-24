21a3a50d091d38298b9aa5d2746f59bca1a66fef
yohanpratamaa <yohanpratamaa@gmail.com>
2025-03-24 10:31:11 +0700

    create file HelloWorld.jsx

---
+++ learn_react/src/hello-world/HelloWorld.jsx
@@ -0,0 +1,9 @@
+export default function HelloWorld() {
+    return (
+        // Di dalam satu funcion hanya bisa mengembalikan satu pembungkus saja
+        <div>
+            <h1>Hello World</h1>
+            <p>Ini adalah pertama belajar react saya</p>
+        </div>
+    )
+}
