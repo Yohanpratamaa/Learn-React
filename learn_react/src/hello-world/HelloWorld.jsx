4228b99d60294af43cefdbfc56efea5af582fe16
yohanpratamaa <yohanpratamaa@gmail.com>
2025-03-24 11:20:41 +0700

    update

--- learn_react/src/hello-world/HelloWorld.jsx
+++ learn_react/src/hello-world/HelloWorld.jsx
@@ -2,8 +2,21 @@ export default function HelloWorld() {
     return (
         // Di dalam satu funcion hanya bisa mengembalikan satu pembungkus saja
         <div>
-            <h1>Hello World</h1>
-            <p>Ini adalah pertama belajar react saya</p>
+            <Header />
+            <Paragraf />
         </div>
+    )
+}
+
+// Untuk Function harus menggunakan huruf besar pada awal
+function Header(){
+    return (
+        <h1>Hello World</h1>
+    )
+}
+
+function Paragraf(){
+    return (
+        <p>Ini adalah pertama belajar react saya dari programmer Yohan</p>
     )
 }
